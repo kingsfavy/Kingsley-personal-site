0c33556bef0c742f6c8b6e798faf9fe212d4a829
kingsfavy <122705122+kingsfavy@users.noreply.github.com>
2023-02-11 10:35:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function show(){
+         document.getElementById("demo").style.display = "block";
+      }
+      function hide(){
+         document.getElementById("demo").style.display = "none";
+      }
+      
+  function getValue(){
+      var n = [];
+      var a = n.slice(0,1);
+      var b = n.slice(1,n.length);
+      var c = a.toUpperCase();
+      var d = b.toLowerCase();
+     var e = c + d;
+    
+}
--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 
-<?=
-$name=$_POST["name"];
-$email=$_POST["email"];
-$message=$_POST["message"];
-echo "welcome: $name,your email is: $email,your message is: $message";
-?>
-<?php
-$host='localhost:3306';
-$name=";
-$email=";
-$message=";
-$conn= mysquli_connect($host, $name, $email, $message);
-if($conn)
-{
-die( 'could not connect:" .mysqli_error());
-}
-echo 'connected successfully';
-mysqli_close($conn);
-?>
+echo "<h2>hello</h2>";
+
+
+$msg="hello world";
+    print "message is;$msg";
